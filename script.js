45a3c16985944f628f1074012da5e939c40085f5
yuresky <122635217+yuresky@users.noreply.github.com>
2023-09-03 15:21:25 -0300

    arrumando ainda

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ window.addEventListener("scroll", function() {
       header.classList.remove("shadows");
     }
   });
-  
+
+
+const colors = ["#007BFF", "#FF5733", "#39CC86", "#E74C3C", "#9B59B6"];
+let currentIndex = 0;
+const wavePattern = document.querySelector('.wave-pattern');
+
+function changeColor() {
+  wavePattern.style.backgroundColor = colors[currentIndex];
+  currentIndex = (currentIndex + 1) % colors.length;
+}
+
+setInterval(changeColor, 2000); // Alterna as cores a cada 2 segundos (ajuste conforme necessário)

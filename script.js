9443634f659651150d73047c79cbea247953c388
yuresky <122635217+yuresky@users.noreply.github.com>
2023-10-06 13:28:05 -0300

    cansei dsa poha
    sfdgghjk

--- script.js
+++ script.js
@@ -22,17 +22,26 @@
 // setInterval(changeColor, 2000); // Alterna as cores a cada 2 segundos (ajuste conforme necessário)
 
 // Função para mostrar o pop-up
-function mostrarPopup() {
-  document.getElementById('popupCadastro').style.display = 'block';
-}
+// function mostrarPopup() {
+//   document.getElementById('popupCadastro').style.display = 'block';
+// }
 
-// Função para fechar o pop-up
-function fecharPopup() {
-  document.getElementById('popupCadastro').style.display = 'none';
-}
+// // Função para fechar o pop-up
+// function fecharPopup() {
+//   document.getElementById('popupCadastro').style.display = 'none';
+// }
 
-// Adicione um ouvinte de evento ao botão de registro para mostrar o pop-up
-document.getElementById('btnMostrarPopup').addEventListener('click', mostrarPopup);
+// // Adicione um ouvinte de evento ao botão de registro para mostrar o pop-up
+// document.getElementById('btnMostrarPopup').addEventListener('click', mostrarPopup);
 
-// Adicione um ouvinte de evento ao botão "Fechar" no pop-up para fechá-lo
-document.getElementById('btnFecharPopup').addEventListener('click', fecharPopup);
+// // Adicione um ouvinte de evento ao botão "Fechar" no pop-up para fechá-lo
+// document.getElementById('btnFecharPopup').addEventListener('click', fecharPopup);
+
+
+window.addEventListener('load', function () {
+  const foto = document.getElementById('foto');
+  const container = document.querySelector('.container');
+
+  // Aumentar o tamanho e rotacionar a foto
+  container.style.transform = 'scale(2) rotate(45deg)';
+});
